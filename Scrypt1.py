c8528ac7245582b4902cda072815da54b08283db
EduardoHF <eduardohf@ciencias.unam.mx>
2024-03-05 23:30:51 -0600

    filtra

--- Scrypt1.py
+++ Scrypt1.py
@@ -33,12 +33,44 @@ def filtraApellidos(cond):
         "SELECT * FROM usuarios WHERE nombre LIKE '% "+cond+"'"
     )
 
-agregaUsuario("Yo Hernandeez","yo.com")
-agregaPelicula("La toalla del mojado","Terror")
-agregaRenta(13,"1",dt.date.today())
+
+
+#agregaUsuario("Yo Hernandeez","yo.com")
+#agregaPelicula("La toalla del mojado","Terror")
+#agregaRenta(13,"1",dt.date.today())
 
 cond = input("Ingrese la cadena con la que desea que el apallido del cliente finalice\n")
 filtraApellidos(cond)
 
 connection.commit()
 connection.close()
+
+def cambiaGenero(nombre_pelicula, nuevo_genero):
+    connection = pymysql.connect(host='localhost',
+                             user='lab',
+                             password='Developer123!',
+                             database='lab_ing_software',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+    cursor = connection.cursor()
+    cursor.execute(
+        "SELECT * FROM peliculas WHERE nombre = %s", nombre_pelicula
+    )
+    pelicula = cursor.fetchone()
+    print(pelicula)
+
+    if pelicula:
+        # La película existe, actualizar su género
+        id_pelicula = (pelicula.get(0))
+        actualizacion = "UPDATE peliculas SET genero = %s WHERE idPelicula = %s"
+        cursor.execute(actualizacion, (nuevo_genero, id_pelicula))
+        connection.commit()
+        print(f"Se ha actualizado el género de la película '{nombre_pelicula}' a '{nuevo_genero}'.")
+    else:
+        print(f"No se encontró ninguna película con el nombre '{nombre_pelicula}'.")
+    
+    connection.close()
+
+
+cambiaGenero("verde","hombre")
+cambiaGenero("La toalla del mojado","hombre")
